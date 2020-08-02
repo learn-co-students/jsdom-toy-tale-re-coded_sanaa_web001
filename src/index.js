adc45de5cc2c7f8490197abd014ab3eaa3ff8b6e
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-08-02 21:51:38 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,32 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
-  const toyCollection=document.getElementById("toy-collection");
+  const toyCollection = document.getElementById("toy-collection");
+  const formAddToy=document.querySelector(".add-toy-form");
+
+  formAddToy.addEventListener("submit",function(e){
+    e.preventDefault();
+    var name=document.querySelectorAll('[name=name]');
+    var srcUrl=document.querySelectorAll('[name=image]');
+    // console.log(name[0].value);
+    configurationObject = {
+      method: "POST",
+      // method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+       
+        name: name[0].value,
+        image: srcUrl[0].value,
+        likes: 0
+      })
+    };
+    fetch(`http://localhost:3000/toys/`, configurationObject);
+
+
+  });
 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -29,80 +54,81 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // fetch("http://localhost:3000/dogs", configurationObject);
 
-  function fetchToys(){
+  function fetchToys() {
     fetch("http://localhost:3000/toys")
-    .then(data =>data.json())
-    .then(dt=>handleToysFetch(dt));
+      .then(data => data.json())
+      .then(dt => handleToysFetch(dt));
   }
 
-  function handleToysFetch(data){
+  function handleToysFetch(data) {
     // data.forEach(element => {
     //   console.log(element.name);
     // });
     // console.log(data.length);
 
     data.forEach(element => {
-        // console.log(element);
-        var card=document.createElement("div");
-        card.classList.add("card");
+      // console.log(element);
+      var card = document.createElement("div");
+      card.classList.add("card");
 
-        var toyName=document.createElement("h2");
-        toyName.innerHTML=element.name;
-       
-        var image=document.createElement("img");
-        image.src=element.image;
-        image.classList.add("toy-avatar");
+      var toyName = document.createElement("h2");
+      toyName.innerHTML = element.name;
 
-        var likes=document.createElement("p");
-        likes.innerHTML=`Toys has <b><u>${element.likes}</u></b> likes.`;
+      var image = document.createElement("img");
+      image.src = element.image;
+      image.classList.add("toy-avatar");
 
-
-       
-        var btnLike=document.createElement("button");
-        btnLike.classList.add("like-btn");
-        btnLike.innerHTML="like";
-        btnLike.addEventListener("click",function(){
-          incrementLikes(element);
-        });
-
-        
+      var likes = document.createElement("p");
+      likes.innerHTML = `Toys has <b><u>${element.likes}</u></b> likes.`;
 
 
-        card.appendChild(toyName);
-        card.appendChild(image);
-        card.appendChild(likes);
-        card.appendChild(btnLike);
-        toyCollection.appendChild(card);
-        
+
+      var btnLike = document.createElement("button");
+      btnLike.classList.add("like-btn");
+      btnLike.innerHTML = "like";
+      btnLike.addEventListener("click", function () {
+        incrementLikes(element);
+      });
+
+
+
+
+      card.appendChild(toyName);
+      card.appendChild(image);
+      card.appendChild(likes);
+      card.appendChild(btnLike);
+      toyCollection.appendChild(card);
+
 
     });
-    
+
   }
-//   h2 tag with the toy's name
-// img tag with the src of the toy's image attribute and the class name "toy-avatar"
-// p tag with how many likes that toy has
-// button tag with a class "like-btn"
+  //   h2 tag with the toy's name
+  // img tag with the src of the toy's image attribute and the class name "toy-avatar"
+  // p tag with how many likes that toy has
+  // button tag with a class "like-btn"
 
-function incrementLikes(elem){
-   configurationObject = {
-    // method: "POST",
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-      id: elem.id,
-      name: elem.name,
-      image: elem.image,
-      likes: elem.likes+1
-    })
-  };
+  function incrementLikes(elem) {
+    configurationObject = {
+      // method: "POST",
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        id: elem.id,
+        name: elem.name,
+        image: elem.image,
+        likes: elem.likes + 1
+      })
+    };
 
-  fetch(`http://localhost:3000/toys/${elem.id}`, configurationObject);
+    fetch(`http://localhost:3000/toys/${elem.id}`, configurationObject);
+  }
 
-}
-  
+
+
   fetchToys();
-  
+
 });
