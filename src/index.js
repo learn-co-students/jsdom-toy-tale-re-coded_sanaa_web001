64d6753ba4f52cb227056084974eb86459369054
Asma Kolaib <as.kolaib@gmail.com>
2020-09-14 23:13:19 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,37 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+
+let addToy = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector("#new-toy-btn");
+  const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.getElementById("toy-collection");
+  const formAddToy=document.querySelector(".add-toy-form");
+
+  formAddToy.addEventListener("submit",function(e){
+    e.preventDefault();
+    var name=document.querySelectorAll('[name=name]');
+    var srcUrl=document.querySelectorAll('[name=image]');
+    // console.log(name[0].value);
+    configurationObject = {
+      method: "POST",
+      // method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+       
+        name: name[0].value,
+        image: srcUrl[0].value,
+        likes: 0
+      })
+    };
+    fetch(`http://localhost:3000/toys/`, configurationObject);
+
+
+  });
+
