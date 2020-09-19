f3a633143efd1566fa84147f204b5cf2372f2a74
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-19 05:55:51 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,23 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetch('http://localhost:3000/toys').then(res=>res.json()).then((data)=>{
+    data.forEach((toy)=>{
+      let card=document.creatElement("div");
+      card.setAttribute("class","card");
+      let h2=document.creatElement("div");
+      h2.innerHTML=toy.name;
+      let img=document.creatElement("img");
+      img.setAttribute("src",toy.toy-avatar);
+      let p=document.creatElement("p");
+      p.innerHTML=toy.likes;
+      let button=document.creatElement("button");
+      button.setAttribute("class","like-btn");
+      card.appendChild(h2);
+      card.appendChild(img);
+      card.appendChild(p);
+      card.appendChild(button);
+      document.getElementById('toy-collection').appendChild(card);
+    })
+  })
 });
